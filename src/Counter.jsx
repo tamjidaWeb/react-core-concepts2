23b7d98af95b677e4a22fa4e7e64a8ec600cfb4e
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-13 00:39:35 +0600

    commited

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 
 export default function Counter (){
-    const [] = useState
+    const abc = useState(0);
+    console.log(abc)
     return (
         <div>
             <h3>Counter:</h3>
