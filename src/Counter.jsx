f2a31a06617b7e10b713c1e3820d4ab00201d107
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-13 00:47:59 +0600

    commited

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,6 +4,8 @@ export default function Counter (){
     const [count , setCount] = useState(0);
 
     const handleAdd = () =>{
+        const newCount = count +1;
+        setCount(newCount)
 
     }
    
