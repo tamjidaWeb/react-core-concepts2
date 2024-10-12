3a169da419ae5724380132411a2cc19856e43942
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-13 00:52:10 +0600

    commited

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -8,11 +8,17 @@ export default function Counter (){
         setCount(newCount)
 
     }
+
+    const handleReduce =()=>{
+        const newCount = count - 1;
+        setCount(newCount);
+    }
    
     return (
         <div style={{border: '2px solid yellow'}}>
             <h3>Counter: {count}</h3>
             <button onClick={handleAdd}>Add</button>
+            <button onClick={handleReduce}>Reduce</button>
         </div>
     )
 }
