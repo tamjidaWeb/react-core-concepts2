6257669b5645db3bf23ee060622084b9a7aaefa7
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-13 12:20:11 +0600

    commited

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
+
 export default function Team(){
+    const  [team, setTeam] = useState(11);
+
+    const handleAdd = () =>{
+        const newTeam = team +1;
+        setTeam(newTeam);
+    }
+    const teamStyle = {
+        border: '2px solid purple',
+        margin: '15px',
+        padding:'15px',
+        borderRadius:'15px',
+       }
     return (
-        <div>
-            <h3>Players:</h3>
+        <div style={teamStyle}>
+            <h3>Players: {team} </h3>
+            <button onClick={handleAdd}>Add</button>
         </div>
     )
 }
