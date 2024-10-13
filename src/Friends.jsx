2650c6e955dfbcd455295e4cd86c9c18bd4d2175
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-13 14:54:43 +0600

    commited

--- src/Friends.jsx
+++ src/Friends.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react'
+import './Friends.css'
 export default function Friends(){
-    <div>
-        <h3>Friends: </h3>
+
+    const [friends, setFriends] = useState([]);
+    useEffect(() =>{
+        fetch ('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setFriends(data))
+
+    },[])
+
+    return (
+        <div className='box'>
+        <h3>Friends: {friends.length}</h3>
+        {
+                friends.map(friend => (<Friend key={friend.id} friend={friend} />))
+            }
     </div>
+    )
 }
+
+
+
+
+// 1.state to hold data
+// 2. use effect with dependency array
+// 3. use fetch to load data
+// 4.set loaded data to the state
+
+
+
+
+
+
+
+
