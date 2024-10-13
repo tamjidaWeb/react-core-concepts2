7b3e7e7a17ca2dbd1d2f61af8301fd59c825622d
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-13 13:18:04 +0600

    commited

--- src/Users.jsx
+++ src/Users.jsx
@@ -2,9 +2,21 @@ import { useState } from "react"
 
 export default function Users(){
     const [users, setUsers] = useState([])
-    return (
+    const abc = () =>{
+
+    }
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, [])
+      
+        return (
         <div>
 
         </div>
     )
 }
+
+// 1. declare a state to hold the data
+//useEffect with call back and dependency array
